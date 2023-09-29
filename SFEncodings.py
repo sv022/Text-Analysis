3a999f4c1a3ab2cec7f2f4b3778ed57c2779bd60
sv022 <83920644+sv022@users.noreply.github.com>
2023-09-29 13:30:13 +0300

    Add files via upload

---
+++ SFEncodings.py
@@ -0,0 +1,87 @@
+from TextAnalysys import *
+
+class Unit:
+    def __init__(self, symbol=None, frequency=0):
+        self.symbol = symbol
+        self.frequency = frequency
+        self.left = None
+        self.right = None
+
+
+def build_shannon_fano_tree(data : list[tuple]) -> Unit:
+    if len(data) == 0:
+        return None
+    if len(data) == 1:
+        return Unit(symbol=data[0][0], frequency=data[0][1])
+
+    total_frequency = sum(item[1] for item in data)
+    half_frequency = total_frequency // 2
+    current_sum = 0
+    index = 0
+
+    for i, (symbol, frequency) in enumerate(data):
+        current_sum += frequency
+        if current_sum >= half_frequency:
+            index = i
+            break
+
+    left_data = data[:index + 1]
+    right_data = data[index + 1:]
+
+    root = Unit()
+    root.left = build_shannon_fano_tree(left_data)
+    root.right = build_shannon_fano_tree(right_data)
+
+    return root
+
+
+def getCodes(root, current_code="", code_dict=None):
+    if code_dict is None:
+        code_dict = {}
+
+    if root.symbol is not None:
+        code_dict[root.symbol] = current_code
+        return code_dict
+
+    getCodes(root.left, current_code + "0", code_dict)
+    getCodes(root.right, current_code + "1", code_dict)
+
+    return code_dict
+
+
+def shannon_fano_encode(data : str) -> (str, dict):
+    symbol_frequency = countTwoLetterSyll(data)
+    sorted_data = sorted(symbol_frequency.items(), key=lambda x: x[1], reverse=True)
+
+    root = build_shannon_fano_tree(sorted_data)
+    codes = getCodes(root)
+
+    encoded_data = "".join([codes[symbol] for symbol in data])
+
+    return encoded_data, codes
+
+
+def shannon_fano_encode_2(data : str) -> (str, dict):
+    symbol_frequency = countTwoLetterSyll(data)
+
+    sorted_data = sorted(symbol_frequency.items(), key=lambda x: x[1], reverse=True)
+
+    root = build_shannon_fano_tree(sorted_data)
+    codes = getCodes(root)
+
+    encoded_data = "".join([codes[symbol] for symbol in symbol_frequency])
+
+    return encoded_data, codes
+
+
+def shannon_fano_decode(encoded_data, codes : dict):
+    decoded_data = ""
+    current_code = ""
+    swapcodes = {val : key for key, val in codes.items()}
+    for bit in encoded_data:
+        current_code += bit
+        if current_code in swapcodes:
+            decoded_data += swapcodes[current_code]
+            current_code = ''
+
+    return decoded_data
---
+++ TextAnalysys.py
@@ -0,0 +1,105 @@
+from string import punctuation, ascii_letters
+from math import log2, ceil
+
+class Unit:
+    def __init__(self, value : str, probability : float, code : str) -> None:
+        self.value = value
+        self.probability = probability
+        self.code = code
+
+
+def showstats(text : str, showall=True):
+    chars = countChars(text)
+    symcount = symCount(text)
+    syll = countTwoLetterSyll(text)
+    output = open('stats.txt', 'w')
+    output.write(f'Sample size: {symcount} character\n')
+    i = 1
+    for x in chars:
+        output.write(f'{x} : {chars[x]}\n')
+        i += 1
+        if not showall and i == 5: break
+    output.write('\n')
+    for x in sorted(syll, key=lambda x: syll[x]):
+        output.write(f'{x} : {syll[x]}\n')
+        if not showall: break
+    output.close()
+
+
+def format(filename : str, remove_whitespace=False) -> None:
+    f = open(filename, 'r')
+    text = f.read().lower()
+    if remove_whitespace:
+        text = text.replace(' ', '')
+    for c in punctuation:
+        text = text.replace(c, '')
+    text = text.replace('\t', '')
+    text = text.replace('\n', '')
+    f.close()
+    g = open(filename.removesuffix('.txt') + '_format.txt', 'w')
+    g.write(text.lower())
+    g.close()
+
+def getItems(chars : dict, symcount : int, code='') -> list[Unit]:
+    items = []
+    for c in chars:
+        i = Unit(c, chars[c] / symcount, code)
+        items.append(i)
+    return items
+    
+
+def symCount(text : str) -> int:
+    return len(text) - text.count(' ')
+
+
+def countChars(text : str, count_whitespace=False) -> dict:
+    chars = {}
+    for c in text:
+        if not(c in ascii_letters):
+            if (c == ' ' and not count_whitespace):
+                continue
+        try:
+            chars[c] += 1
+        except Exception:
+            chars[c] = 1
+    return chars
+
+
+def countTwoLetterSyll(text : str, count_whitespace=False) -> dict:
+    text = text.replace(' ', '')
+    syll = dict()
+    for i in range(0, len(text) - 1, 2):
+        if not(text[i] in ascii_letters and text[i + 1] in ascii_letters):
+            continue
+        try:
+            syll[text[i] + text[i + 1]] += 1
+        except Exception:
+            syll[text[i] + text[i + 1]] = 1
+    if len(text) % 2 != 0: syll[text[-1] + ' '] = 2
+    return syll
+
+
+def getEntropyPerChar(chars : dict, symcount : int) -> float:
+    chardist = {}
+    for c in chars:
+        chardist[c] = chars[c] / symcount
+    entropyPerLetter = 0
+    for c in chardist:
+        entropyPerLetter += (-chardist[c] * log2(chardist[c]))
+    return entropyPerLetter
+
+
+def entropyPerTwoLetterSyll(syll : dict, syllCount : int) -> float:
+    entropyTwoLetter = 0
+    for c in syll:
+        p = syll[c] / syllCount
+        entropyTwoLetter += (-log2(p) * p)
+    return entropyTwoLetter
+
+
+def minCodeLen(chars : dict) -> int:
+    return ceil(log2(len(chars)))
+
+
+def codeExcessiveness(entropy : float, symcount : int) -> float:
+    return  1 - (log2(entropy) / log2(symcount))
